d2f10defc5d6b218629fbfabe292fe5a2e630225
Sudeepreddy1106 <venkatasudeepreddy2006@gmail.com>
2026-06-28 09:42:48 +0000

    Sync LeetCode submission - Sort Colors - Runtime - 0 ms (100.00%), Memory - 43.5 MB (42.22%)

--- DSA/0075-sort-colors/solution.java
+++ DSA/0075-sort-colors/solution.java
@@ -1,31 +1,25 @@
 class Solution {
     public void sortColors(int[] nums) {
-     int count0=0;
-     int count1=0;
-     int count2=0;
-     for(int i=0;i<nums.length;i++){
-        if(nums[i]==0){
-            count0++;
+    int left=0;
+    int mid= 0;
+    int right=nums.length-1;
+    while(mid<=right){
+        if(nums[mid]==1){
+            mid++;
         }
-        else if(nums[i]==1){
-            count1++;
-        }
-        else {
-            count2++;
-        }
-     }  
-     for(int i=0;i<nums.length;i++){
-        if(i<count0){
-            nums[i]=0;
-        }
-        else if(i<count1+count0){
-            nums[i]=1;
+        else if(nums[mid]==0){
+            int temp = nums[left];
+            nums[left]=nums[mid];
+            nums[mid]=temp;
+            mid++;
+            left++;
         }
         else{
-            nums[i]=2;
+            int temp = nums[right];
+            nums[right]=nums[mid];
+            nums[mid]=temp;
+            right--;
         }
-
-     }
-
+    }
     }
 }
