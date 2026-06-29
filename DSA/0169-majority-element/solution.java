5830bf1d3dc5c8b4458032ebbf811ade19bcd66a
Sudeepreddy1106 <venkatasudeepreddy2006@gmail.com>
2026-06-29 16:24:51 +0000

    Sync LeetCode submission - Majority Element - Runtime - 1 ms (99.89%), Memory - 55.6 MB (46.26%)

---
+++ DSA/0169-majority-element/solution.java
@@ -0,0 +1,20 @@
+ class Solution {
+    public int majorityElement(int[] nums) {
+    int count=1;
+    int num=nums[0];
+    for(int i=1;i<nums.length;i++){
+        if(nums[i]==num){
+            count++;
+        }
+        else if(num!=nums[i]){
+            count--;
+            if(count==0){
+            num=nums[i];
+            count++;
+            }
+        }
+       
+    }
+    return num;
+    }
+}
