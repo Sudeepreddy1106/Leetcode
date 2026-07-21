bf23c44704428495426504e5d544dfb8a6a34ece
Sudeepreddy1106 <venkatasudeepreddy2006@gmail.com>
2026-07-21 17:22:43 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation - Runtime - 6 ms (87.85%), Memory - 45.4 MB (46.68%)

---
+++ DSA/0150-evaluate-reverse-polish-notation/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].equals("+")) {
+                int a = st.pop();
+                int b = (st.pop());
+                st.push((a + b));
+            } else if (tokens[i].equals("-")) {
+                int a = (st.pop());
+                int b = (st.pop());
+                st.push((b-a));
+            } else if (tokens[i].equals("*")) {
+                int a = (st.pop());
+                int b =(st.pop());
+                st.push((a * b));
+            } else if (tokens[i].equals("/")) {
+                int a = (st.pop());
+                int b = (st.pop());
+                st.push((b/a));
+            }
+            else{
+                st.push(Integer.parseInt(tokens[i]));
+            }
+        }
+        int ret= st.pop();
+        return ret;
+    }
+}
+
